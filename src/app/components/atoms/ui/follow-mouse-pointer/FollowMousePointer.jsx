1f92ed062847dbfd430c40597bdf24e93a8dde90
Eduardo <ejrb1234@gmail.com>
2023-12-28 16:16:54 -0400

    FollowMousePointer, finished ✅

--- src/app/components/atoms/ui/follow-mouse-pointer/FollowMousePointer.jsx
+++ src/app/components/atoms/ui/follow-mouse-pointer/FollowMousePointer.jsx
@@ -25,42 +25,41 @@ export const FollowMousePointer = () => {
 
   return (
     <>
-      <div
-        className={styles.circle}
-        style={{
-          position: 'absolute',
-          // backgroundColor: '#111',
-          // backdropFilter: 'invert(100%)',
-          borderRadius: '50%',
-          border: '2px solid #fff',
-          pointerEvents: 'none',
-          left: -28,
-          top: -28,
-          width: 52,
-          height: 52,
-          transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
-          opacity: `${opacity}`,
-          transition: `opacity 1s ease`,
-
-          willChange: 'transform',
-          transformStyle: 'preserve-3d',
-        }}
-      />
-      <div
-        className={styles['center-point']}
-        style={{
-          position: 'absolute',
-          left: -6,
-          top: -6,
-          width: 6,
-          height: 6,
-          borderRadius: '50%',
-          backgroundColor: '#fff',
-          transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
-          opacity: `${opacity}`,
-          transition: `opacity 1s ease`,
-        }}
-      />
+      <div className={styles.cursor}>
+        <div
+          className={styles.circle}
+          style={{
+            position: 'absolute',
+            borderRadius: '50%',
+            border: '2px solid #fff',
+            pointerEvents: 'none',
+            left: -28,
+            top: -28,
+            width: 52,
+            height: 52,
+            transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
+            opacity: `${opacity}`,
+            transition: `opacity 1s ease`,
+            willChange: 'transform',
+            transformStyle: 'preserve-3d',
+          }}
+        />
+        <div
+          className={styles['center-point']}
+          style={{
+            position: 'absolute',
+            left: -6,
+            top: -6,
+            width: 6,
+            height: 6,
+            borderRadius: '50%',
+            backgroundColor: '#fff',
+            transform: `translate(${mousePosition.x}px, ${mousePosition.y}px)`,
+            opacity: `${opacity}`,
+            transition: `opacity 1s ease`,
+          }}
+        />
+      </div>
     </>
   )
 }
