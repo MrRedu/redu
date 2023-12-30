e130ace89b2eab65af136de84151ecb386bb84ed
Eduardo <ejrb1234@gmail.com>
2023-12-30 19:20:22 -0400

    working on CardConnect (component) ✅

--- src/app/components/molecules/card-connect/CardConnect.jsx
+++ src/app/components/molecules/card-connect/CardConnect.jsx
@@ -1,15 +1,24 @@
 import { Section } from '@/components/atoms/ui/section/Section'
 import styles from './CardConnect.module.css'
 import { Button } from '@/components/atoms/ui/buttons/Button'
+import { ArrowRight } from 'lucide-react'
 export const CardConnect = () => {
   return (
     <Section>
-      <div className={styles['card-connect']}>
+      <div className={styles.container}>
         <p className={styles['helper-text']}>Have a project in mind?</p>
         <p className={styles.title}>Let’s Connect</p>
-        <Button href={'/contact'} type={'primary'}>
+        <Button
+          href="/contact"
+          type="primary"
+          size="large"
+          rightIcon={<ArrowRight size={18} />}
+        >
           Learn More
         </Button>
+
+        <div className={`${styles['bg-circle']} ${styles['bg-circle-1']}`} />
+        <div className={`${styles['bg-circle']} ${styles['bg-circle-2']}`} />
       </div>
     </Section>
   )
