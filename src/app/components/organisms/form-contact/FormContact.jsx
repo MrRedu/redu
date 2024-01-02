eeb301f9bb150eb40b9de27d7f8e573807f5f457
Eduardo <ejrb1234@gmail.com>
2024-01-01 23:56:37 -0400

    Form implementation

--- src/app/components/organisms/form-contact/FormContact.jsx
+++ src/app/components/organisms/form-contact/FormContact.jsx
@@ -1,44 +1,93 @@
+'use client'
+
 import styles from './FormContact.module.css'
+
+import { useForm } from '@/hooks/useForm'
+
 import { GroupForm } from './GroupForm'
-
-import { Input } from './Input'
 import { Label } from './Label'
+import { Input } from './Input'
 import { TextArea } from './TextArea'
+import { MultipleSelect } from '../../molecules/multiple-select/MultipleSelect'
 
-const SERVICES = ['Web Design', 'Landing Pages', 'Branding', 'Packaging']
+// const SERVICES_OFFERED = [
+//   'Web Design',
+//   'Landing Pages',
+//   'Branding',
+//   'Packaging',
+// ]
 
 export const FormContact = () => {
+  const formDataInitialState = {
+    name: '',
+    email: '',
+    companyName: '',
+    services: [],
+    message: '',
+  }
+  const { formData, handleChange, handleSubmit } = useForm(formDataInitialState)
+
   return (
-    <form action="" className={styles.form}>
+    <form
+      action=""
+      className={styles.form}
+      onSubmit={handleSubmit}
+      autoComplete="off"
+    >
       <GroupForm>
         <Label htmlFor="name" isRequired>{`What's your name?`}</Label>
-        <Input type="text" id="name" placeholder="Eduardo" isRequired />
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          handleChange={handleChange}
+          value={formData.name}
+          placeholder="Eduardo"
+          isRequired
+        />
       </GroupForm>
       <GroupForm>
         <Label htmlFor="email" isRequired>{`How can I contact you?`}</Label>
         <Input
           type="email"
           id="email"
+          name="email"
+          handleChange={handleChange}
+          value={formData.email}
           placeholder="your@email.com"
           isRequired
         />
       </GroupForm>
       <GroupForm>
         <Label
-          htmlFor="company"
+          htmlFor="companyName"
           isRequired
         >{`What is the name of you business?`}</Label>
-        <Input type="text" id="company" placeholder="Company name" isRequired />
+        <Input
+          type="text"
+          id="companyName"
+          name="companyName"
+          handleChange={handleChange}
+          value={formData.companyName}
+          placeholder="Company name"
+          isRequired
+        />
       </GroupForm>
       <GroupForm>
         <Label htmlFor="" isRequired>{`What services do you need?`}</Label>
-        <ul className={styles['list-services']}>
-          {SERVICES.map(service => (
+        {/* <ul className={styles['list-services']}>
+          {formData.services.map(service => (
             <li key={service} className={styles['item-list']}>
               {service}
             </li>
           ))}
-        </ul>
+        </ul> */}
+        {/* <select>
+          {SERVICES_OFFERED.map(service => (
+            <option key={service}>{service}</option>
+          ))}
+        </select> */}
+        <MultipleSelect />
       </GroupForm>
       <GroupForm>
         <Label
@@ -46,13 +95,18 @@ export const FormContact = () => {
           isRequired
         >{`Anything else you would like to share?`}</Label>
         <TextArea
-          placeholder="Share projects details"
           id="details"
+          name="message"
+          handleChange={handleChange}
+          value={formData.message}
+          placeholder="Share projects details"
           isRequired
         ></TextArea>
       </GroupForm>
       <GroupForm>
         <button
+          type="submit"
+          onClick={handleSubmit}
           style={{
             minHeight: '44px',
             borderRadius: 'var(--main-border-radius)',
