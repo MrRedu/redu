19a70db594aba983b5639ee875bebf9c2573a542
Eduardo <ejrb1234@gmail.com>
2024-01-25 00:22:19 -0400

    new hero section

--- src/app/components/molecules/ui/hero-section/HeroSection.jsx
+++ src/app/components/molecules/ui/hero-section/HeroSection.jsx
@@ -1,50 +1,24 @@
 import styles from './HeroSection.module.css'
-import Image from 'next/image'
+import { Github, Linkedin, Briefcase } from 'lucide-react'
 export const HeroSection = () => {
   return (
     <>
       <section className={styles.container}>
-        <div
-          className={`${styles['image-container-1']} ${styles['image-container']}`}
-        >
-          <Image
-            src={'/projects/gyphi.webp'}
-            alt=""
-            width={800}
-            height={100}
-            className={`${styles.image} ${styles['image-1']}`}
-          />
-        </div>
-        <h2 className={`${styles.text} ${styles['text-1']}`}>Where Vision</h2>
-        <div
-          className={`${styles['image-container-2']} ${styles['image-container']}`}
-        >
-          <Image
-            src={'/projects/gyphi.webp'}
-            alt=""
-            width={800}
-            height={100}
-            className={`${styles.image} ${styles['image-2']}`}
-          />
-        </div>
-
-        <span className={`${styles.text} ${styles['text-2']}`}>Meets</span>
-
-        <div
-          className={`${styles['image-container-3']} ${styles['image-container']}`}
-        >
-          <Image
-            src={'/projects/gyphi.webp'}
-            alt=""
-            width={800}
-            height={100}
-            className={`${styles.image} ${styles['image-3']}`}
-          />
-        </div>
-
-        <span className={`${styles.text} ${styles['text-3']}`}>
-          Digital Reality
-        </span>
+        <span className={styles.name}>Eduardo R.</span>
+        <h2 className={styles.title}>Frontend Developer</h2>
+        <ul className={styles['social-list']}>
+          <li className={styles['social-item']}>
+            <Github size={36} />
+          </li>
+          <li className={styles['social-item']}>
+            <Linkedin size={36} />
+          </li>
+          <li className={styles['social-item']}>
+            <Briefcase size={36} />
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
       </section>
     </>
   )
