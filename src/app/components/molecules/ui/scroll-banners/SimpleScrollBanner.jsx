6a4efbd7afca21dd85ea57a01ef716f96d56ad74
Eduardo <ejrb1234@gmail.com>
2024-01-24 22:07:34 -0400

    fixed SimpleScrollBanner component

--- src/app/components/molecules/ui/scroll-banners/SimpleScrollBanner.jsx
+++ src/app/components/molecules/ui/scroll-banners/SimpleScrollBanner.jsx
@@ -1,21 +1,24 @@
 'use client'
+
+import propTypes from 'prop-types'
+
 import { ScrollBanner } from '@/components/molecules/ui/infinite-scroll-banner/InfiniteScrollBanner'
 
 import { SCROLL_ITEMS } from '@/utils/constants'
 
-export const DoubleScrollBanner = () => {
+export const SimpleScrollBanner = ({ direction, speed }) => {
   return (
     <ScrollBanner>
       <ScrollBanner.InfiniteScrollBanner
         items={SCROLL_ITEMS}
-        direction="right"
-        speed="slow"
-      />
-      <ScrollBanner.InfiniteScrollBanner
-        items={SCROLL_ITEMS}
-        direction="left"
-        speed="fast"
+        direction={direction}
+        speed={speed}
       />
     </ScrollBanner>
   )
+}
+
+SimpleScrollBanner.propTypes = {
+  direction: propTypes.string,
+  speed: propTypes.string,
 }
