39b7172920e102b31eafb6d7ae02f9087e1347b7
Eduardo <ejrb1234@gmail.com>
2024-02-03 13:46:43 -0400

    new ScrollDown component

--- src/app/components/molecules/ui/hero-section/HeroSection.jsx
+++ src/app/components/molecules/ui/hero-section/HeroSection.jsx
@@ -13,44 +13,39 @@ export const HeroSection = () => {
   const isScrolled = useScrolled()
 
   return (
-    <>
-      <section className={styles.container}>
-        <div className={styles['content-container']}>
-          <div className={styles.content}>
-            <span className={styles.arrow}>
-              <ArrowDownRight size={32} />
-            </span>
-            <span className={`${styles.text} ${styles.status}`}>
-              #opentowork
-            </span>
-            <span className={`${styles.text} ${styles.title}`}>
-              Web developer
-            </span>
-          </div>
+    <section className={styles.container}>
+      <div className={styles['content-container']}>
+        <div className={styles.content}>
+          <span className={styles.arrow}>
+            <ArrowDownRight size={32} />
+          </span>
+          <span className={`${styles.text} ${styles.status}`}>#opentowork</span>
+          <span className={`${styles.text} ${styles.title}`}>
+            Web developer
+          </span>
         </div>
+      </div>
 
-        <div
-          className={styles['scroll-down-container']}
-          style={{
-            opacity: isScrolled ? 1 : 0,
-            transition: 'opacity 0.6s ease',
-          }}
-          onClick={() => smoothScroll('about-me')}
-        >
-          <ScrollDown />
-          <span className={styles['helper-text']}>Scroll Down</span>
-        </div>
+      <div
+        className={styles['scroll-down-container']}
+        style={{
+          opacity: isScrolled ? 1 : 0,
+          transition: 'opacity 0.6s ease',
+        }}
+        onClick={() => smoothScroll('about-me')}
+      >
+        <ScrollDown />
+      </div>
 
-        <div className={styles['image-container']}>
-          <Image
-            src={'/avatar.png'}
-            width={1200}
-            height={800}
-            alt="Redu"
-            className={styles.image}
-          />
-        </div>
-      </section>
-    </>
+      <div className={styles['image-container']}>
+        <Image
+          src={'/avatar.png'}
+          width={1200}
+          height={800}
+          alt="Redu"
+          className={styles.image}
+        />
+      </div>
+    </section>
   )
 }
