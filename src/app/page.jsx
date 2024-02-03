2f810b10ca88912012ee94465fbb972fdd41ce83
Eduardo <ejrb1234@gmail.com>
2024-02-03 17:56:34 -0400

    Selected works (component)

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,15 @@
 import { ServicesSection } from '@/components/organisms/services-section/ServicesSection'
 import { CardConnect } from '@/components/molecules/card-connect/CardConnect'
 import { AboutMeCard } from '@/components/organisms/about-me-card/AboutMeCard'
-import { HeroSection } from '#/src/app/components/organisms/ui/hero-section/HeroSection'
+import { HeroSection } from '@/components/organisms/ui/hero-section/HeroSection'
+import { SelectedWorks } from '#/src/app/components/organisms/selected-works/SelectedWorks'
 import {
   SimpleScrollBanner,
   DoubleScrollBanner,
 } from '@/components/molecules/ui/scroll-banners'
 
 export const metadata = {
-  title: 'Portfolio | Redu',
+  title: 'Redu',
 }
 
 export default function HomePage() {
@@ -17,6 +18,7 @@ export default function HomePage() {
       <HeroSection />
       <SimpleScrollBanner direction={'left'} speed={'slow'} />
       <AboutMeCard />
+      <SelectedWorks />
       <ServicesSection />
       <DoubleScrollBanner />
       <CardConnect />
