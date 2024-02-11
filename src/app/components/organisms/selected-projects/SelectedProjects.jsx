e010d216c7ad47364e8c6895a64cb5e7a205e7a3
Eduardo <ejrb1234@gmail.com>
2024-02-10 22:20:20 -0400

    Function for sorting SelectedProjects

--- src/app/components/organisms/selected-projects/SelectedProjects.jsx
+++ src/app/components/organisms/selected-projects/SelectedProjects.jsx
@@ -7,21 +7,34 @@ import { SelectedProject } from './SelectedProject'
 import { CategoryTitle } from '@/components/molecules/category-title/CategoryTitle'
 import { Button } from '@/components/atoms/ui/buttons/Button'
 
+const ID_SELECTED_PROJECTS = [
+  'fun-with-flags',
+  'gyphi',
+  'tidi-list',
+  'another-project',
+]
+
+const SELECTED_PROJECTS = projectsData.sort((a, b) => {
+  return ID_SELECTED_PROJECTS.indexOf(a.id) - ID_SELECTED_PROJECTS.indexOf(b.id)
+})
+
 export const SelectedProjects = () => {
   return (
     <>
       <CategoryTitle>Selected projects</CategoryTitle>
       <div className={styles.container}>
-        {projectsData.map(({ id, title, internUrl, images, roles, year }) => (
-          <SelectedProject
-            key={id}
-            title={title}
-            internUrl={internUrl}
-            roles={roles}
-            year={year}
-            image={images[0]}
-          />
-        ))}
+        {SELECTED_PROJECTS.map(
+          ({ id, title, internUrl, images, roles, year }) => (
+            <SelectedProject
+              key={id}
+              title={title}
+              internUrl={internUrl}
+              roles={roles}
+              year={year}
+              image={images[0]}
+            />
+          )
+        )}
         <Button>
           <Button.LinkWrapper href="/projects">
             <Button.Text>View all projects</Button.Text>
