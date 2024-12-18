ef619c2c48f9283384ed6f15e27bf119c303bb7b
Eduardo <ejrb1234@gmail.com>
2024-12-17 21:53:47 -0400

    🤺

--- src/app/components/organisms/about-me-card/AboutMeCard.jsx
+++ src/app/components/organisms/about-me-card/AboutMeCard.jsx
@@ -1,7 +1,5 @@
 import styles from './AboutMeCard.module.css'
-
 import { CategoryTitle } from '@/components/molecules/category-title/CategoryTitle'
-
 import { ProfileCard } from '@/components/molecules/profile-card/ProfileCard'
 import { ProfileInformation } from '@/components/molecules/profile-information/ProfileInformation'
 import { SocialMedia } from '@/components/molecules/social-media/SocialMedia'
